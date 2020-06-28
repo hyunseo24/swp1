2de3747c079a7abc6c752ee236a924952f666a15
hyunseo24 <66055665+hyunseo24@users.noreply.github.com>
2020-06-28 22:45:53 +0900

    이슈#(2-2)

---
+++ calc/config.py
@@ -0,0 +1,10 @@
+WSGIScriptAlias /fig /Git/hyunseo24/swp1
+WSGIPythonPath /Git/hyunseo24/swp1
+
+<Directory "/Git/hyunseo24/swp1">
+    AllowOverride None
+    Order deny,allow
+    Require all granted
+</Directory>
+
+이슈#(2-2)
